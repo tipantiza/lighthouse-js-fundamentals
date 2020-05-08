3ef0a78408c59de4db5a5ebaa0a98210afb550a5
landon tipantiza <landontipantiza@gmail.com>
2020-05-08 20:04:34 +0000

    calculates age

---
+++ age-calc.js
@@ -0,0 +1,4 @@
+var ageCalculator = function(name, yearOfBirth, currentYear){
+ return name + " is " + (currentYear - yearOfBirth) +" years old.";
+}
+console.log(ageCalculator("Landon", 2000, 2020));
